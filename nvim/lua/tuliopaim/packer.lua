110f4e01c10469f9809e4a8eeddb2e9644825e5c
tuliopaim <tutpaim@gmail.com>
2023-04-09 19:58:42 -0300

    update markdownpreview and readme

--- nvim/lua/tuliopaim/packer.lua
+++ nvim/lua/tuliopaim/packer.lua
@@ -72,9 +72,9 @@ return require('packer').startup(function(use)
     }
 
     -- Markdown
-    use({ "iamcco/markdown-preview.nvim",
-        run = "cd app && npm install",
-        setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
-        ft = { "markdown" }, })
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
 
 end)
