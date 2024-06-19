cf55f719ea7864f9c8f05a0ff540440319a25bf7
tuliopaim <tutpaim@gmail.com>
2024-06-18 21:22:33 -0300

    hyprland configs update after nix

---
+++ hypr/.config/hypr/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Wallpaper
+swww init &
+swww img ~/Images/blobs-d.png &
+
+# Network indicator
+nm-applet --indicator &
+
+waybar &
+
+# Clipboard
+copyq --start-server & 
+copyq config hide_main_window true
--- symlinks.sh
+++ symlinks.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
 stow -v -t /home/tuliopaim/ -d /home/tuliopaim/.dotfiles/ \
     hypr waybar swaylock wlogout\
-    git zsh tmux nvim alacritty \
-    ideavim scripts \
+    git scripts nvim alacritty \
+    ideavim \
