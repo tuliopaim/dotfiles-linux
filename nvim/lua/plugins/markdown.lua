9096b8803f43186f005361c36a26e3fcc3b3ad2a
tuliopaim <tutpaim@gmail.com>
2024-08-22 20:08:51 -0300

    update markdown

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,12 +1,9 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    ft = "markdown",
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
-	cmd = {
-		"MarkdownPreviewToggle",
-		"MarkdownPreview",
-		"MarkdownPreviewStop",
-	},
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
 }
