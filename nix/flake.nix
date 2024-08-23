387ad60837ff5ff4ae48aac885df8d6caadde926
tuliopaim <tutpaim@gmail.com>
2024-08-23 13:29:41 -0300

    unfree pkgs at flake level

--- nix/flake.nix
+++ nix/flake.nix
@@ -20,8 +20,8 @@
     let
       inherit (self) outputs;
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      pkgs-unstable = import nixpkgs-unstable { inherit system; config.allowUnfree = true; };
     in
     {
       nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
