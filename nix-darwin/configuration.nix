57a82e8a749d1ee59031d616b7770665b8138573
tuliopaim <tutpaim@gmail.com>
2024-11-17 15:46:50 -0300

    MACOS: extract configuration to a separated nix file

---
+++ nix-darwin/configuration.nix
@@ -0,0 +1,41 @@
+{ pkgs, outputs, ... }:
+{
+  environment.systemPackages =
+    [
+      pkgs.vim
+      pkgs.neovim
+      pkgs.ansible
+      pkgs.mkalias
+    ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+    liberation_ttf
+  ];
+
+  services.nix-daemon.enable = true;
+
+  nix.settings.experimental-features = "nix-command flakes";
+
+  system.configurationRevision = outputs.rev or outputs.dirtyRev or null;
+
+  system.stateVersion = 5;
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  users.users.tuliopaim.home = "/Users/tuliopaim";
+  security.pam.enableSudoTouchIdAuth = false;
+  home-manager.backupFileExtension = "backup";
+  nix.configureBuildUsers = true;
+  nix.useDaemon = true;
+
+  system.defaults = {
+    dock.autohide = true;
+    dock.mru-spaces = false;
+    finder.AppleShowAllExtensions = true;
+    finder.FXPreferredViewStyle = "clmv";
+    loginwindow.LoginwindowText = "tuliopaim";
+    loginwindow.GuestEnabled = false;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark";
+  };
+}
