2098b3451d370b127b14f41ad499a042b002cd01
tuliopaim <tutpaim@gmail.com>
2025-02-15 23:39:58 -0300

    reclace neo-tree, lazygit, autopairs and telescope by snacks

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,14 +1,5 @@
 return {
     {
-        "kdheepak/lazygit.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            vim.keymap.set('n', "<leader>lg", function() require('lazygit').lazygit() end)
-        end
-    },
-    {
         'tpope/vim-fugitive'
     },
     {
