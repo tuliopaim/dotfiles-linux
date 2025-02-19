ee50464fad2be525197385690bef02cc21736192
tuliopaim <tutpaim@gmail.com>
2025-02-18 23:05:43 -0300

    add tmux windownizer

--- nix/home-manager/terminal/scripts.nix
+++ nix/home-manager/terminal/scripts.nix
@@ -4,6 +4,7 @@
     (pkgs.writeShellScriptBin "clone-wt" (builtins.readFile ../../../scripts/clone-wt))
     (pkgs.writeShellScriptBin "prune-wt" (builtins.readFile ../../../scripts/prune-wt))
     (pkgs.writeShellScriptBin "tmux-sessionizer" (builtins.readFile ../../../scripts/tmux-sessionizer))
+    (pkgs.writeShellScriptBin "tmux-windownizer" (builtins.readFile ../../../scripts/tmux-windownizer))
     (pkgs.writeShellScriptBin "tmux-dotfiles" (builtins.readFile ../../../scripts/tmux-dotfiles))
     (pkgs.writeShellScriptBin "usersecrets" (builtins.readFile ../../../scripts/usersecrets))
     (pkgs.writeShellScriptBin "lockscreentime" (builtins.readFile ../../../scripts/lockscreentime))
