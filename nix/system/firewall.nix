0dd1d463e0f6ca7cf09fd75a5b66e6654392c3d0
tuliopaim <tutpaim@gmail.com>
2025-02-18 23:05:43 -0300

    whitelist for local connections

--- nix/system/firewall.nix
+++ nix/system/firewall.nix
@@ -1,5 +1,10 @@
 { pkgs, ... }:
 {
   networking.firewall.enable = true;
-  services.fail2ban.enable = true;
+  services.fail2ban = {
+    enable = true;
+    ignoreIP = [
+        "192.168.100.12"
+    ];
+  };
 }
