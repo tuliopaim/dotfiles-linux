5b0d99f41d55ac3f89af8588d3374304c4568970
tuliopaim <tutpaim@gmail.com>
2025-02-18 23:05:43 -0300

    update telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -65,24 +65,22 @@ return {
 
             -- find files
 
-            vim.keymap.set('n', '<leader>ff', function()
-                builtin.find_files(
-                {
-                    layout_strategy='vertical',
-                    hidden= true
-                })
-            end);
-            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            local vertical_files = {
+                layout_strategy='vertical',
+                hidden = true
+            };
 
-            -- search word
+            vim.keymap.set('n', '<leader>ff', function() builtin.find_files(vertical_files) end);
+            vim.keymap.set('n', '<leader>fg', function() builtin.git_files(vertical_files) end, {})
+            vim.keymap.set('n', '<leader>s.', function() builtin.oldfiles(vertical_files) end, { desc = '[S]earch Recent Files ("." for repeat)' })
+            vim.keymap.set('n', '<leader>sg', function() builtin.live_grep(vertical_files) end, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader><leader>', function() builtin.buffers(vertical_files) end, { desc = '[ ] Find existing buffers' })
+
             vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = "[?] [S]earch [H]elp" })
             vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
             vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
             vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
             vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-            vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-            vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
             vim.keymap.set('n', '<leader>fws', function()
                 local word = vim.fn.expand("<cword>")
