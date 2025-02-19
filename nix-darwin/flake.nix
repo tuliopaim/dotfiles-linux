f6e79413891717e1ccd4c2c3f84e37f88c92ca9f
tuliopaim <tutpaim@gmail.com>
2025-02-18 23:05:44 -0300

    MACOS: extract configuration to a separated nix file

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -6,6 +6,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,50 +25,17 @@
       system = "aarch64-darwin";
       pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
       pkgs-unstable = import inputs.nixpkgs-unstable { inherit system; config.allowUnfree = true; };
-      configuration = { pkgs, ... }: {
-
-        environment.systemPackages =
-          [
-            pkgs.vim
-            pkgs.neovim
-            pkgs.ansible
-          ];
-
-        fonts.packages = with pkgs; [
-          (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-          liberation_ttf
-        ];
-
-        services.nix-daemon.enable = true;
-
-        nix.settings.experimental-features = "nix-command flakes";
-
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        system.stateVersion = 5;
-
-        nixpkgs.hostPlatform = "aarch64-darwin";
-
-        users.users.tuliopaim.home = "/Users/tuliopaim";
-        security.pam.enableSudoTouchIdAuth = false;
-        home-manager.backupFileExtension = "backup";
-        nix.configureBuildUsers = true;
-        nix.useDaemon = true;
-
-        system.defaults = {
-          dock.autohide = true;
-          dock.mru-spaces = false;
-          finder.AppleShowAllExtensions = true;
-          finder.FXPreferredViewStyle = "clmv";
-          loginwindow.LoginwindowText = "tuliopaim";
-        };
-      };
     in
     {
       darwinConfigurations."macos" = nix-darwin.lib.darwinSystem {
+        inherit pkgs;
         system = "aarch64-darwin";
+        specialArgs = {
+          inherit outputs self;
+        };
         modules = [
-          configuration
+          ./configuration.nix
+
           home-manager.darwinModules.home-manager
           {
             home-manager.extraSpecialArgs = {
