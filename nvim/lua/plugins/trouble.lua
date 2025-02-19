37671472c4d9b68f7d34d241afe8ab52db2fcb51
tuliopaim <tutpaim@gmail.com>
2025-02-18 23:05:44 -0300

    reclace neo-tree, lazygit, autopairs and telescope by snacks

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,13 +1,36 @@
 return {
     "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
     keys = {
         {
-            "<leader>tx",
+            "<leader>xx",
             "<cmd>Trouble diagnostics toggle<cr>",
             desc = "Diagnostics (Trouble)",
-        }
-    }
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+    },
+    specs = {
+        "folke/snacks.nvim",
+        opts = function(_, opts)
+            return vim.tbl_deep_extend("force", opts or {}, {
+                picker = {
+                    actions = require("trouble.sources.snacks").actions,
+                    win = {
+                        input = {
+                            keys = {
+                                ["<c-t>"] = {
+                                    "trouble_open",
+                                    mode = { "n", "i" },
+                                },
+                            },
+                        }
+                    },
+                },
+            })
+        end,
+    },
 }
