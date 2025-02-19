51fa8f92fb92481869567a228e58bf5baf8e0ef9
tuliopaim <tutpaim@gmail.com>
2025-02-18 23:05:43 -0300

    use my flake for zen

--- nix/flake.nix
+++ nix/flake.nix
@@ -8,7 +8,7 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    zen-browser.url = "github:tuliopaim/zen-browser-flake";
   };
 
   outputs =
