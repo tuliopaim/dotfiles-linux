ad3652ca3ee2026ecc018b3c056ed6c2cdad400c
tuliopaim <tutpaim@gmail.com>
2025-02-18 23:05:42 -0300

    add tmux-dotfiles script

--- nix/home-manager/terminal/scripts.nix
+++ nix/home-manager/terminal/scripts.nix
@@ -4,6 +4,7 @@
     (pkgs.writeShellScriptBin "clone-wt" (builtins.readFile ../../../scripts/clone-wt))
     (pkgs.writeShellScriptBin "prune-wt" (builtins.readFile ../../../scripts/prune-wt))
     (pkgs.writeShellScriptBin "tmux-sessionizer" (builtins.readFile ../../../scripts/tmux-sessionizer))
+    (pkgs.writeShellScriptBin "tmux-dotfiles" (builtins.readFile ../../../scripts/tmux-dotfiles))
     (pkgs.writeShellScriptBin "usersecrets" (builtins.readFile ../../../scripts/usersecrets))
   ];
 }
