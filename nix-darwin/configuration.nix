a3d7f1012a2fc14186c15a74b29e2bcdb70f9c1e
tuliopaim <tutpaim@gmail.com>
2025-11-18 21:38:43 -0300

    migrate brew to nix

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -38,4 +38,98 @@
     loginwindow.GuestEnabled = false;
     NSGlobalDomain.AppleInterfaceStyle = "Dark";
   };
+
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+      # 'zap': Uninstalls all formulae(and casks) not listed here.
+      cleanup = "zap";
+    };
+
+    taps = [
+      "mongodb/brew"
+      "nikitabobko/tap"
+    ];
+
+    brews = [
+      "mas"
+      "zsh"
+      "zsh-autosuggestions"
+      "zsh-vi-mode"
+      "zsh-syntax-highlighting"
+      "tmux"
+      "mono-libgdiplus"
+      "python@3.13"
+      "cask"
+      "fnm"
+      { name = "libpq"; link = true; }
+      "mongosh"
+      "sevenzip"
+      "yazi"
+      "poetry"
+      "typescript"
+      "clipboard"
+      "ansible"
+      "zlib"
+      "make"
+      "direnv"
+      "awscli"
+      "awscli-local"
+      "hg"
+      "imagemagick"
+      "qmk/qmk/qmk"
+      "angular-cli"
+      "redis"
+      "gdu"
+    ];
+
+    # GUI Apps
+    casks = [
+      "aerospace"
+      "balenaetcher"
+      "bitwarden"
+      "brave-browser"
+      "cursor"
+      "dbeaver-community"
+      "discord"
+      "drawio"
+      "firefox"
+      "font-fira-code-nerd-font"
+      "font-jetbrains-mono-nerd-font"
+      "ghostty"
+      "google-chrome"
+      "istat-menus"
+      "karabiner-elements"
+      "localsend"
+      "logi-options+"
+      "microsoft-auto-update"
+      "microsoft-edge"
+      "microsoft-teams"
+      "mongodb-compass"
+      "obs"
+      "obsidian"
+      "pdf-expert"
+      "pgadmin4"
+      "postman"
+      "qmk-toolbox"
+      "raycast"
+      "rider"
+      "scroll-reverser"
+      "shottr"
+      "slack"
+      "soundsource"
+      "spotify"
+      "via"
+      "visual-studio-code"
+      "wezterm"
+    ];
+
+    # Mac App Store Apps (requires 'mas' in brews)
+    masApps = {
+      # "Xcode" = 497799835;
+    };
+  };
 }
