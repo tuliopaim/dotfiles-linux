098b3827a6bb9c2181d666451aeae078fe1fd6d1
tuliopaim <tutpaim@gmail.com>
2026-03-18 17:33:03 -0300

    merge tmux-sessionizer and windownizer into tmux-worktree

--- nix/nixos/home-manager/terminal/scripts.nix
+++ nix/nixos/home-manager/terminal/scripts.nix
@@ -3,8 +3,7 @@
   home.packages = [
     (pkgs.writeShellScriptBin "clone-wt" (builtins.readFile ../../../scripts/clone-wt))
     (pkgs.writeShellScriptBin "prune-wt" (builtins.readFile ../../../scripts/prune-wt))
-    (pkgs.writeShellScriptBin "tmux-sessionizer" (builtins.readFile ../../../scripts/tmux-sessionizer))
-    (pkgs.writeShellScriptBin "tmux-windownizer" (builtins.readFile ../../../scripts/tmux-windownizer))
+    (pkgs.writeShellScriptBin "tmux-worktree" (builtins.readFile ../../../scripts/tmux-worktree))
     (pkgs.writeShellScriptBin "tmux-dotfiles" (builtins.readFile ../../../scripts/tmux-dotfiles))
     (pkgs.writeShellScriptBin "tmux-vault" (builtins.readFile ../../../scripts/tmux-vault))
     (pkgs.writeShellScriptBin "usersecrets" (builtins.readFile ../../../scripts/usersecrets))
