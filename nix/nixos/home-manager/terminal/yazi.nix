ddee90aac8ecd780609396907b1749d1a8d6caef
tuliopaim <tutpaim@gmail.com>
2026-04-21 08:22:14 -0300

    fix yazi

--- nix/nixos/home-manager/terminal/yazi.nix
+++ nix/nixos/home-manager/terminal/yazi.nix
@@ -2,10 +2,11 @@
 {
   programs.yazi = {
     enable = true;
+    shellWrapperName = "y";
     settings = {
-      manager = {
+      mgr = {
         show_hidden = true;
-        sort_by = "modified";
+        sort_by = "mtime";
         sort_dir_first = true;
         sort_reverse = true;
       };
