1bd30dd1f6f25942cbdd473daa3995e28a8bd370
tuliopaim <tutpaim@gmail.com>
2026-09-11 18:29:32 -0300

    feat(camera): launch standalone preview app

---
+++ vicinae/scripts/open-camera.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# @vicinae.schemaVersion 1
+# @vicinae.title Open Camera
+# @vicinae.mode silent
+# @vicinae.icon 📷
+# @vicinae.description Open the native Camera Preview app
+# @vicinae.keywords ["camera", "webcam", "photo"]
+
+open "$HOME/Applications/Camera Preview.app"
